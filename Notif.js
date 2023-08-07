6949b6471750758b7daabf8f90cced1fc1a44b30
Fadil <117158618+SaiaFadil@users.noreply.github.com>
2023-08-07 09:03:55 +0700

    Delete Notif.js

--- Notif.js
+++
@@ -1,9 +0,0 @@
-var toastTrigger = document.getElementById('liveToastBtn')
-var toastNotif = document.getElementById('liveToast')
-if (toastTrigger) {
-    toastTrigger.addEventListener('click', function () {
-        var toast = new bootstrap.Toast(toastNotif)
-
-        toast.show()
-    })
-}
